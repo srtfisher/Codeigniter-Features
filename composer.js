900276f1ece6a82fa462dc71e0c40d212684fd67
Sean Fisher <srtfisher@gmail.com>
2012-12-03 18:53:04 -0500

    Composer and Spark Update

---
+++ composer.js
@@ -0,0 +1,21 @@
+{
+  "name": "srtfisher/Codeigniter-Features",
+  "description": "Codeigniter Feature Flags",
+  "keywords": [
+    "codeigniter",
+    "feature flag",
+    "features",
+	"library"
+  ],
+  "homepage": "https://seanfisher.co/",
+  "license": "MIT",
+  "authors": [
+    {
+      "name": "Sean Fisher",
+      "homepage": "http://seanfisher.co/"
+    }
+  ],
+  "require": {
+    "php": ">=5.2",
+  }
+}
